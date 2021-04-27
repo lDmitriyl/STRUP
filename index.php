4328fca76dac6f47c17ed4ccbe5b0b9fdaf0bdee
Dmitiy Glebov <webb17@yandex.ru>
2021-04-27 15:36:55 +0300

    add: Task Test of Strup

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+$data = ['red', 'blue', 'green', 'yellow', 'lime', 'magenta', 'black', 'gold', 'gray', 'tomato'];
+
+function strup($data)
+{
+    $res = '<div>';
+
+    for ($i = 0; $i < 5; $i++) {
+
+        $rand_keys = array_rand($data, 5);
+
+        foreach ($rand_keys as $key) {
+
+            $color =  randomColor($data, $data[$key]);
+
+            $res .= '<p style="color:' . $color. '; display: inline-block">' . $data[$key] . '</p>' . " ";
+
+        }
+        $res .= '<br>';
+    }
+
+    $res .= '</div>';
+
+    return $res;
+}
+
+$res = strup($data);
+
+echo $res;
+
+function randomColor($data, $exceptColor){
+
+    do{
+        $randColor = array_rand($data, 1);
+
+        $color = $data[$randColor];
+
+    }while ($data[$randColor] == $exceptColor);
+
+    return $color;
+
+}
+
+?>
